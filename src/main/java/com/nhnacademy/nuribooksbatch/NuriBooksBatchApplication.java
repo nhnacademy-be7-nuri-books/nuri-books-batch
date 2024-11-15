fad9f97117a4f2fd3f524959a325078f86bd6b07
kkyongg <kkyongg2@gmail.com>
2024-11-15 10:17:52 +0900

    commit

---
+++ src/main/java/com/nhnacademy/nuribooksbatch/NuriBooksBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nuribooksbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriBooksBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuriBooksBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/nuribooksbatch/NuriBooksBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.nuribooksbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriBooksBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
